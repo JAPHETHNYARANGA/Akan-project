cda7f52f24faddd26ae9d421047910b3c227ad48
JAPHETHNYARANGA <nyaranga4@gmail.com>
2021-07-18 23:46:01 +0300

    used alert instead of dom elements to display results

--- java.js
+++ java.js
@@ -39,6 +39,7 @@ close.addEventListener('click', ()=>{
         var YY =parseInt(year.substring(2,4));
         var MM = parseInt(document.getElementById('birthmonth').value);
         var DD = parseInt(document.getElementById('birthday').value);
+       
         
     
         ans = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
@@ -60,12 +61,11 @@ close.addEventListener('click', ()=>{
         }
     }
     
-    function printNames(){
-        var First = document.getElementById('first-name').value;
-        var Last = document.getElementById('last-name').value;
-    }
+   
 
 function getGender(){
+    var First = document.getElementById('first-name').value;
+    var Last = document.getElementById('last-name').value;
        
     var genders = document.getElementsByName("gender");
     if(genders[0].checked == true){
@@ -80,47 +80,47 @@ function getGender(){
       switch(gender){
         case "male":
           if (day1 == 1){
-            document.getElementById("textR").innerHTML="You were born on " +days[0] + " and Your akan name is " +male[0];
+            alert(First + Last +" You were born on " +days[0] + " and Your akan name is " +male[0]);
           }
           else if(day1 == 2){
-            document.getElementById("textR").innerHTML=" You were born on "+days[1] + " and Your akan name is " +male[1];
+            alert(First + Last +" You were born on "+days[1] + " and Your akan name is " +male[1]);
           }
           else if(day1 == 3){
-            document.getElementById("textR").innerHTML=" You were born on " +days[2]+ " and Your akan name is " +male[2];
+            alert(First + Last +" You were born on " +days[2]+ " and Your akan name is " +male[2]);
           }
           else if(day1 == 4){
-            document.getElementById("textR").innerHTML=" You were born on "+days[3] +  " and Your akan name is " +male[3];
+            alert(First + Last +" You were born on "+days[3] +  " and Your akan name is " +male[3]);
           }
           else if(day1 == 5){
-            document.getElementById("textR").innerHTML=" You were born on "+days[4] +  " and Your akan name is " +male[4];
+            alert(First + Last +" You were born on "+days[4] +  " and Your akan name is " +male[4]);
           }
           else if(day1 == 6){
-            document.getElementById("textR").innerHTML=" You were born on "+days[5] +  " and Your akan name is " +male[5];
+            alert(First + Last +" You were born on "+days[5] +  " and Your akan name is " +male[5]);
           }
           else if(day1 == -0){
-            document.getElementById("textR").innerHTML=" You were born on "+days[6] + " and Your akan name is " +male[6];
+            alert(First + Last +" You were born on "+days[6] + " and Your akan name is " +male[6]);
           }
         break;
         case "female":
           if (day1 == 1){
-            document.getElementById("textR").innerHTML=" You were born on "+days[0] + " and Your akan name is  " +female[0];
+            alert(First + Last +" You were born on "+days[0] + " and Your akan name is  " +female[0]);
           }
           else if(day1 == 2){
-            document.getElementById("textR").innerHTML=" You were born on " +days[1] + " and Your akan name is " +female[1];
+           alert(First + Last +" You were born on " +days[1] + " and Your akan name is " +female[1]);
           }
           else if(day1 == 3){
-            document.getElementById("textR").innerHTML=" You were born on " +days[2] + " and Your akan name is " +female[2];
+            alert(First + Last +" You were born on " +days[2] + " and Your akan name is " +female[2]);
           }
           else if(day1 == 4){
-            document.getElementById("textR").innerHTML=" You were born on " +days[3] + " and Your akan name is " +female[3];
+            alert(First + Last +" You were born on " +days[3] + " and Your akan name is " +female[3]);
           }
           else if(day1 == 5){
-            document.getElementById("textR").innerHTML=" You were born on " +days[4] + " and Your akan name is " +female[4];
+            alert(First + Last +" You were born on " +days[4] + " and Your akan name is " +female[4]);
           }
           else if(day1 == 6){
-            document.getElementById("textR").innerHTML=" You were born on " +days[5] + " and Your akan name is " + female[5];
+            alert(First + Last +" You were born on " +days[5] + " and Your akan name is " + female[5]);
           }else if(day1 == -0){
-            document.getElementById("textR").innerHTML=" You were born on " +days[6] + " and Your akan name is " +female[6];
+            alert(First + Last +" You were born on " +days[6] + " and Your akan name is " +female[6]);
           }
         break
         default:
